585800b57cf8d6f8f08861b6a5c70419ae44ccd7
Berserker04 <52007143+Berserker04@users.noreply.github.com>
2022-10-21 00:11:51 -0500

    Add MUI

--- src/components/layouts/Layout.jsx
+++ src/components/layouts/Layout.jsx
@@ -1,22 +1,43 @@
+import { Grid } from "@mui/material";
 import React from "react";
 import { Outlet } from "react-router-dom";
 import { SomeSkill } from "../someSkills/SomeSkill";
 import { Slider } from "./components/slider/Slider";
 
-import "./styles.scss"
+import "./styles.scss";
 
 export const Layout = () => {
   return (
-    <div className="container">
-      <div>
+    <Grid className="container" container spacing={2}>
+      <Grid
+        item
+        direction="row"
+        justifyContent="flex-end"
+        alignItems="center"
+        xs={12}
+        md={3}
+      >
         <Slider />
-      </div>
-      <div className="main">
-        <Outlet />
-      </div>
-      <div>
-        <SomeSkill />
-      </div>
-    </div>
+      </Grid>
+      <Grid item xs={12} md={6}>
+        {/* <Outlet /> */}
+        <p>2</p>
+      </Grid>
+      <Grid item xs={12} md={3}>
+        {/* <SomeSkill /> */}
+        <p>3</p>
+      </Grid>
+    </Grid>
+    // <div className="container">
+    //   <div>
+    //     <Slider />
+    //   </div>
+    //   <div className="main">
+    //     <Outlet />
+    //   </div>
+    //   <div>
+    //     <SomeSkill />
+    //   </div>
+    // </div>
   );
 };
